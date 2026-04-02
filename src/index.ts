567711862a9774a8f11c39e6a4e256666ee69018
Sara-Barbosa-Silva <sara.barbosa.silvas@gmail.com>
2026-04-02 19:54:17 -0300

    feat: aplica diferentes status às consultas

--- src/index.ts
+++ src/index.ts
@@ -149,3 +149,12 @@ const consulta2 = criarConsulta(2, medico2, paciente2, new Date(2026, 0, 1), 450
 const consulta3 = criarConsulta(3, medico3, paciente3, new Date(2026, 1, 15), 300);
 const consulta4 = criarConsulta(4, medico1, paciente2, new Date(2026, 2, 10), 400);
 const consulta5 = criarConsulta(5, medico2, paciente1, new Date(2026, 3, 20), 500);
+
+const consulta2Confirmada = confirmarConsulta(consulta2);
+
+const consulta3Cancelada = cancelarConsulta(consulta3);
+
+const consulta4Realizada: Consulta = {
+  ...consulta4,
+  status: "realizada",
+};
